5052a4a172d418f4c7c51fd48ad672793b9076ac
Milks, Brianna Rae <bmilks3@gatech.edu>
2022-09-08 21:38:29 -0400

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,7 +2,7 @@
 
 mkdir -p data
 cd data
-gdown "https://drive.google.com/file/d/1pzTS_lKAHOvoum5cIp9-7KVqpNOoJo-z/view?usp=sharing"
+gdown "https://drive.google.com/uc?id=1pzTS_lKAHOvoum5cIp9-7KVqpNOoJo-z"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
