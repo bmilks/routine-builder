c87da43ec11272d3bf587a9131a3fe56b5a4189d
Milks, Brianna Rae <bmilks3@gatech.edu>
2022-09-09 14:02:53 -0400

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,10 +2,10 @@
 
 mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?id=1pzTS_lKAHOvoum5cIp9-7KVqpNOoJo-z"
+gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
-mv data/vibe_data/suni.mp4 .
+mv data/vibe_data/sample_video.mp4 .
 mkdir -p $HOME/.torch/models/
 mv data/vibe_data/yolov3.weights $HOME/.torch/models/
